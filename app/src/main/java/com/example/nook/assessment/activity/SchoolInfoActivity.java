ba5794878abb4acd4187fd9099247d5bc67183ad
Kieartbodin <kieartbodin.cha@ascencorp.com>
2017-09-25 19:43:42 +0700

    Spinner for province

--- app/src/main/java/com/example/nook/assessment/activity/SchoolInfoActivity.java
+++ app/src/main/java/com/example/nook/assessment/activity/SchoolInfoActivity.java
@@ -6,11 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
 import com.example.nook.assessment.R;
+import com.example.nook.assessment.adapter.ProvinceSpinnerAdapter;
 import com.example.nook.assessment.manager.CFAS;
 
 public class SchoolInfoActivity extends Activity {
@@ -21,13 +25,16 @@ public class SchoolInfoActivity extends Activity {
     EditText departmentName;
     EditText addressName;
     EditText districtName;
-    EditText province;
+    Spinner province;
     EditText studentCount;
     EditText customerCount;
     EditText operationCount;
     EditText assessorName;
     EditText assessmentCount;
     EditText assessmentDate;
+
+    String provinceValue;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,15 +51,31 @@ public class SchoolInfoActivity extends Activity {
         departmentName = (EditText) findViewById(R.id.departmentName);
         addressName = (EditText) findViewById(R.id.addressName);
         districtName = (EditText) findViewById(R.id.districtName);
-        province = (EditText) findViewById(R.id.province);
+        province = (Spinner) findViewById(R.id.province);
         studentCount = (EditText) findViewById(R.id.studentCount);
         customerCount = (EditText) findViewById(R.id.customerCount);
         operationCount = (EditText) findViewById(R.id.operationCount);
         assessorName = (EditText) findViewById(R.id.assessorName);
         assessmentCount = (EditText) findViewById(R.id.assessmentCount);
         assessmentDate = (EditText) findViewById(R.id.assessmentDate);
+        districtName.setEnabled(false);
 
 
+        SpinnerAdapter adapter = new ProvinceSpinnerAdapter(this,
+                android.R.layout.simple_spinner_item,
+                CFAS.getInstance().provinces);
+        province.setAdapter(adapter);
+        province.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                provinceValue = (String) province.getSelectedItem();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     public void onClickNextFromMain(View view) {
@@ -69,6 +92,7 @@ public class SchoolInfoActivity extends Activity {
         startActivity(intent);
     }
 
+
     private void saveState() {
 
         CFAS.getInstance().getSchool().setPlaceName(placeName.getText().toString());
@@ -76,7 +100,7 @@ public class SchoolInfoActivity extends Activity {
         CFAS.getInstance().getSchool().setOwnerName(ownerName.getText().toString());
         CFAS.getInstance().getSchool().setDepartmentName(departmentName.getText().toString());
         CFAS.getInstance().getSchool().setDistrict(districtName.getText().toString());
-        CFAS.getInstance().getSchool().setProvince(province.getText().toString());
+        CFAS.getInstance().getSchool().setProvince(provinceValue != null ? provinceValue : null);
         CFAS.getInstance().getSchool().setAddressName(addressName.getText().toString());
         CFAS.getInstance().getSchool().setStudentCount(studentCount.getText().toString());
         CFAS.getInstance().getSchool().setCustomerCount(customerCount.getText().toString());
@@ -86,5 +110,6 @@ public class SchoolInfoActivity extends Activity {
         CFAS.getInstance().getSchool().setAssessmentDate(assessmentDate.getText().toString());
 
     }
+
 
 }
--- app/src/main/java/com/example/nook/assessment/model/SimpleModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.nook.assessment.model;
-
-/**
- * Created by kieartbodin on 9/13/2017 AD.
- */
-public class SimpleModel {
-}
